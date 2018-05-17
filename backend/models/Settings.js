88cf2e6ab64aa94998b3101d935f9f03823c3cfa
Jose Manuel Garcia Sosa <jose.garcia@Optima.local>
2018-05-17 16:54:00 +0200

    models and controllers jose

--- backend/models/Settings.js
+++ backend/models/Settings.js
@@ -2,17 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const settingsSchema=new Schema({
-    nightStartTime:Date,
-    nightEndTime: Date,
+    nightStartTime:String,
+    nightEndTime: String,
     onCallWeekMoney:Double,
+    onCallWeekendMoney:Double,
     
     //before
     beforeNTWeekMoney:Double,
-    beforeNTWeekTime:Date,
+    beforeNTWeekTime:Double,
+    beforeNTWeekendMoney:Double,
+    beforeNTWeekendTime:Double,
+
 
     //after
+    afterNTWeekMoney:Double,
+    afterNTWeekTime:Double,
     afterNTWeekendMoney:Double,
-    afterNTWeekendTime:Date
+    afterNTWeekendTime:Double
     //---------------------//
 
 })
