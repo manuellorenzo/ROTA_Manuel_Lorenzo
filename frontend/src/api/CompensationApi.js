d36b681f59c9b78baf03b937878457dbca8dd7e2
Jose Manuel Garcia Sosa <jose.garcia@Optima.local>
2018-05-23 16:51:48 +0200

    modal

--- frontend/src/api/CompensationApi.js
+++ frontend/src/api/CompensationApi.js
@@ -1,14 +1,17 @@
+import * as urls from '../api/urls';
+
+
 class CompensationApi{
     static getAllCompensations(){
-        return fetch('172.22.5.145:3003/compensation/getCompensations').then(response=>{
-            return response.json;
+        return fetch(urls.URL_JOSE+'/compensation/getCompensations').then(response=>{
+            return response.json();
         }).catch(error=>{
             return (error);
         });
     }
 
     static findCompensationByWorker(_id){
-        return fetch('172.22.5.145:3003/compensation/getCompensationByWorker/'+_id, {
+        return fetch(urls.URL_JOSE+'/compensation/getCompensationByWorker/'+_id, {
             method: 'get',
             headers: {
                 'Accept': 'application/json',
