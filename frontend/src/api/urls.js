8918277c3c9dfacb3c95bb8b5a5d0c415dafb3eb
Manuel <manuel.lorenzo@optimaonline.es>
2018-06-01 11:09:57 +0200

    Changed urls

--- frontend/src/api/urls.js
+++ frontend/src/api/urls.js
@@ -1,3 +1,3 @@
-export const URL_JOSE = "http://172.22.5.145:3964";
+export const URL_JOSE = "http://localhost:3964";//"http://172.22.5.145:3964";
 
-export const URL_FERN = "http://172.22.4.117:3964";
+export const URL_FERN = "http://localhost:3964";//"http://172.22.4.117:3964";
